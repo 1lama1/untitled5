555ffe746d27686cdb43b87043109636b511e62e
user <fatkat100@mail.ru>
2022-07-09 14:46:09 +0300

    вывод надписи генерация рандома

--- src/swe.java
+++ src/swe.java
@@ -1,9 +1,9 @@
 public class swe {
         public static void main(String[] args) {
+            int x = (int)(Math.random() * 100 + 1);
             System.out.println("Люк я твой отец");
             System.out.println("неееееет");
             System.out.println("познай силу");
-
-
+            System.out.println(x);
         }
     }
