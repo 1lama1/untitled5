186620e072704be4d5b0b87ee923dcaaba92c85a
user <fatkat100@mail.ru>
2022-07-09 14:03:54 +0300

    вывод надписи

---
+++ src/swe.java
@@ -0,0 +1,9 @@
+public class swe {
+        public static void main(String[] args) {
+            System.out.println("Люк я твой отец");
+            System.out.println("неееееет");
+            System.out.println("познай силу");
+
+
+        }
+    }
